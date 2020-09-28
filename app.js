f19c8ef8b664a40a1a52253cc3756adad1e51ff6
Ryan <facebook0209@gmail.com>
2020-09-28 14:37:27 +0800

    return to origin

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ function list(path){
 	     文件
 	<i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
 	    </div> 
-	    <div class="mdui-col-sm-3 mdui-text-right" onclick="sortListDirDate()">
+	    <div class="mdui-col-sm-3 mdui-text-right">
 	     修改時間
 	<i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i>
 	    </div> 
@@ -416,61 +416,6 @@ function markdown(el, data){
 window.onpopstate = function(){
     var path = window.location.pathname;
     render(path);
-}
-
-function sortListDirDate() {
-    var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
-    list = document.getElementById("list");
-    switching = true;
-    // Set the sorting direction to ascending:
-    dir = "asc";
-    // Make a loop that will continue until no switching has been done:
-    while (switching) {
-        // Start by saying: no switching is done:
-        switching = false;
-        b = list.getElementsByTagName("LI");
-        // Loop through all list-items:
-        for (i = 0; i < (b.length - 1); i++) {
-            // Start by saying there should be no switching:
-            shouldSwitch = false;
-            /* Check if the next item should switch place with the current item,
-            based on the sorting direction (asc or desc): */
-            if (dir == "asc") {
-                var sortele1 = b[i].getElementByID("sortdate");
-                var sortele2 = b[i + 1].getElementByID("sortdate");
-                if (Number(sortele1.innerHTML) > Number(sortele2.innerHTML)) {
-                    /* If next item is alphabetically lower than current item,
-                    mark as a switch and break the loop: */
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                var sortele1 = b[i].getElementByID("sortdate");
-                var sortele2 = b[i + 1].getElementByID("sortdate");
-                if (Number(sortele1.innerHTML) < Number(sortele2.innerHTML)) {
-                    /* If next item is alphabetically higher than current item,
-                    mark as a switch and break the loop: */
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-        if (shouldSwitch) {
-            /* If a switch has been marked, make the switch
-            and mark that a switch has been done: */
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
-            switching = true;
-            // Each time a switch is done, increase switchcount by 1:
-            switchcount++;
-        } else {
-            /* If no switching has been done AND the direction is "asc",
-            set the direction to "desc" and run the while loop again. */
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
-        }
-    }
 }
 
 
