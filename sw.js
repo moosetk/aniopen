709d019cdee7564e718d879a2bfaca395b99f0e9
RyanL-29 <48479346+RyanL-29@users.noreply.github.com>
2020-12-09 18:51:27 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e=> {
+	e.waitUntil(
+	caches.open("static").then(cache => {
+		return cache.addAll(["./", "https://cdn.jsdelivr.net/gh/RyanL-29/aniopen/aniopen.png", "https://cdn.jsdelivr.net/gh/RyanL-29/aniopen/telegram.png", "https://cdn.jsdelivr.net/gh/RyanL-29/aniopen/app.js", "https://cdn.jsdelivr.net/gh/RyanL-29/aniopen/favicon.ico"]);
+	})
+	);
+});
+
+self.addEventListener("fetch", e => {
+	console.log(`Intercepting fetch request for: ${e.request.url}`);
+	
+});
+
