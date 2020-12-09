e568a2168837ad579b756384e7137a86ac9d048b
RyanL-29 <48479346+RyanL-29@users.noreply.github.com>
2020-12-09 20:26:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener("install", e=> {
 });
 
 self.addEventListener("fetch", e => {
-	console.log(`Intercepting fetch request for: ${e.request.url}`);
+	//console.log(`Intercepting fetch request for: ${e.request.url}`);
 	
 });
 
