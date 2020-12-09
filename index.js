1467f02e9176af75a62ff2b3c12a27f53337ea06
RyanL-29 <48479346+RyanL-29@users.noreply.github.com>
2020-12-09 20:24:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator)
 {
-	navigator.serviceWorker.register("https://cdn.jsdelivr.net/gh/RyanL-29/aniopen/sw.js").then(registration => {
+	navigator.serviceWorker.register("sw.js").then(registration => {
 		console.log("SW Registered!");
 		console.log(registration);
 	}).catch(error => {
