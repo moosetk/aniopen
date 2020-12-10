9e3d64001f0cb2743aaab717893fafe75819ee57
RyanL-29 <48479346+RyanL-29@users.noreply.github.com>
2020-12-10 22:38:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 if ("serviceWorker" in navigator)
 {
 	navigator.serviceWorker.register("sw.js").then(registration => {
-		//console.log("SW Registered!");
-		//console.log(registration);
+		registration.onupdatefound = () => {
+					const installingWorker = registration.installing;
+					installingWorker.onstatechange = () => {
+						switch (installingWorker.state) {
+							case 'installed':
+								if (navigator.serviceWorker.controller) {
+									// new update available
+									resolve(true);
+								} else {
+									// no update available
+									resolve(false);
+								}
+								break;
+						}
+					};
+				};
 	}).catch(error => {
 		//console.log("SW Registration Failed!");
 		//console.log(error);
